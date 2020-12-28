aa7ee4ef88cc2f9d50a34bed3b122a0851fb94f0
weiherr <weiherrteng@gmail.com>
2020-12-28 17:13:41 +0800

    itinerary

---
+++ app/src/main/java/com/example/travelog/Itinerary.java
@@ -0,0 +1,12 @@
+package com.example.travelog;
+
+import android.os.Bundle;
+
+public class Itinerary {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.itinerary);
+    }
+}
