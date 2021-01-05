fc68b73362c0731a8957fcbc3e45e9daa6be426c
weiherr <weiherrteng@gmail.com>
2021-01-06 03:01:55 +0800

    itinerary update

--- app/src/main/java/com/example/travelog/ui/Trips/TripsFragment.java
+++ app/src/main/java/com/example/travelog/ui/Trips/TripsFragment.java
@@ -1,22 +1,29 @@
 package com.example.travelog.ui.Trips;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.travelog.Itinerary_detail;
 import com.example.travelog.R;
+import com.example.travelog.change_password_Activity;
 
 public class TripsFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    private Button trip_plan;
 
-        return inflater.inflate(R.layout.fragment_trips, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_itinerary_detail, container, false);
 
+
+        return view;
     }
 }
