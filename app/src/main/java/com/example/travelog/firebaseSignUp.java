785ba6f2a36ec89254177e6d3f95602b7fe3b5ff
Chi Hng <chihng1332@gmail.com>
2021-01-07 16:01:35 +0800

    Connect user to firebase database

---
+++ app/src/main/java/com/example/travelog/firebaseSignUp.java
@@ -0,0 +1,4 @@
+package com.example.travelog;
+
+public class firebaseSignUp {
+}
---
+++ app/src/main/java/com/example/travelog/firebaseUser.java
@@ -0,0 +1,35 @@
+package com.example.travelog;
+
+public class firebaseUser {
+    String userID, username, password;
+
+    public firebaseUser(String userID, String username, String password) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
