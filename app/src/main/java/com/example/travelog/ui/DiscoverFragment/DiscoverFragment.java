2fc7139f4a6beb4198f173110e361b37c3991ec8
Chi Hng <chihng1332@gmail.com>
2021-01-09 22:19:29 +0800

    Connect user to firebase database

--- app/src/main/java/com/example/travelog/ui/DiscoverFragment/DiscoverFragment.java
+++ app/src/main/java/com/example/travelog/ui/DiscoverFragment/DiscoverFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +17,7 @@ public class DiscoverFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
 
         View view = inflater.inflate(R.layout.fragment_discover, container, false);
     return view;
