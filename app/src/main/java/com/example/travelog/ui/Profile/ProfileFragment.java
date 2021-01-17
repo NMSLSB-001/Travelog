3eb593d6d33ab257d3aa9ed0fb9a5c785259c8bf
weiherr <weiherrteng@gmail.com>
2021-01-17 17:37:16 +0800

    update

--- app/src/main/java/com/example/travelog/ui/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/travelog/ui/Profile/ProfileFragment.java
@@ -53,7 +53,6 @@ public class ProfileFragment extends Fragment {
     private ImageView mImgView;
 
 
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
